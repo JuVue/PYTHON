7cec1a5811157a67c0f08cd30225c73cb43008d6
JuVue <78793837+JuVue@users.noreply.github.com>
2021-04-21 11:21:53 +0100

    created tmp test script

---
+++ tmp_test.py
@@ -0,0 +1,13 @@
+## test SCRIPT
+import datetime
+
+import sys
+sys.path.insert(1, '../py_functions/')
+from time_functions import datenum2date, date2datenum
+
+
+datenum=719529.5
+d=datetime.datetime(1970,1,1,12)
+
+print(datenum2date(datenum))
+print(date2datenum(d))
