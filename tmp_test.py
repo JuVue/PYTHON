98fc2afe184572451f6092965c5c4f33000f2e10
JuVue <78793837+JuVue@users.noreply.github.com>
2021-04-21 13:40:08 +0100

    changed datenum to calculate

--- tmp_test.py
+++ tmp_test.py
@@ -5,8 +5,8 @@ sys.path.insert(1, './py_functions/')
 from time_functions import datenum2date, date2datenum
 
 
-datenum=719529.5
-d=datetime.datetime(1970,1,1,12)
+datenum=719529.5374
+d=datetime.datetime(1970,1,1,12,3,4,3)
 
 dt2d = datenum2date(datenum)
 print(dt2d)
