bb657504324f2ef67f48a691a0281df0f45996e7
JuVue <78793837+JuVue@users.noreply.github.com>
2021-06-04 14:42:26 +0100

    ..

--- use_allCloudnetData_jutta.py
+++ use_allCloudnetData_jutta.py
@@ -7278,7 +7278,7 @@ def interpCloudnet(obs_data):
         times = np.copy(obs_data['time'].data)
         height = np.copy(obs_data['height'])        ### height array constant in time, so just take first column
         nans,id=nanhelper(cv)
-        for i in range(0,len(height)):
+        for i in range(0,len(height)-1):
             print(i)
             tmp=id(~nans[:,i])
             idtmp=np.squeeze(np.nonzero(np.diff(np.append([0],tmp))>3))
