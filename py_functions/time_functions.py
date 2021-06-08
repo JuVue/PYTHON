f9b8f09d24dee5096867ae9024f2ae900f23bef3
JuVue <78793837+JuVue@users.noreply.github.com>
2021-06-08 09:55:50 +0100

    now using pandas fpr datenum2date conversion

--- py_functions/time_functions.py
+++ py_functions/time_functions.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 from datetime import datetime as dt
 from datetime import timedelta
+import panda as pd
 
 def date2datenum(d):
  ##  Convert python datetime to Matlab datenum
@@ -22,38 +23,18 @@ def datenum2date(datenum):
    ##Convert Matlab datenum into Python datetime.
    ##:input  datenum Date in datenum format
    ##:return:Datetime object corresponding to datenum.
-    if isinstance(datenum,'list'):
-        for m in range(0,len(d)):
-            d=datenum[m]
-            days = d % 1
-            hours = days % 1 * 24
-            minutes = hours % 1 * 60
-            seconds = minutes % 1 * 60
-            mseconds = seconds % 1 * 60
-            d = np.array(d)
-            dout[m] = dt.fromordinal(int(d.astype(int))) \
-                   + timedelta(days=int(days)) \
-                   + timedelta(hours=int(hours)) \
-                   + timedelta(minutes=int(minutes)) \
-                   + timedelta(seconds=int(seconds)) \
-                   + timedelta(milliseconds=round(mseconds)) \
-                   - timedelta(days=366)
-    else:
-        d=datenum
-        days = d % 1
-        hours = days % 1 * 24
-        minutes = hours % 1 * 60
-        seconds = minutes % 1 * 60
-        mseconds = seconds % 1 * 60
-        d = np.array(d)
-        dout = dt.fromordinal(int(d.astype(int))) \
-                + timedelta(days=int(days)) \
-                + timedelta(hours=int(hours)) \
-                + timedelta(minutes=int(minutes)) \
-                + timedelta(seconds=int(seconds)) \
-                + timedelta(milliseconds=round(mseconds)) \
-                - timedelta(days=366)
-    return dout
+   d=np.array(datenum)
+   return python_time = pd.to_datetime(d-719529,unit='D')
+    # if isinstance(datenum,list):
+    #     for m in range(0,len(datenum)):
+    #         d=datenum[m]
+    #         d = np.array(d)
+    #         dout[m] =np.append(dout, dt.fromordinal(int(d.astype(int)))+timedelta(days=d%1)-timedelta(days=366))
+    # else:
+    #     d=datenum
+    #     d = np.array(d)
+    #     dout[0] =  dt.fromordinal(int(d.astype(int)))+timedelta(days=d%1)-timedelta(days=366))
+    # return dout
 
 
 def calcTime_Mat2DOY(matlab_time):
