3aed0a3a4c1f007025a30146bff36578195fc301
JuVue <78793837+JuVue@users.noreply.github.com>
2021-06-29 08:15:58 +0100

    correction in removespinup input

--- use_allCloudnetData_jutta.py
+++ use_allCloudnetData_jutta.py
@@ -6445,7 +6445,7 @@ def setFlags(obs_data, um_data, misc_data, ra2t_data, obs_var_list, um_var_list,
 
     return obs_data, um_data, misc_data,  ra2t_data
 
-def removeSpinUp(monc_data):
+def removeSpinUp(monc_data,monc_spin):
 
     monc_var_list = list(monc_data.keys())
     monc_var_list.remove('time1')
@@ -6781,14 +6781,14 @@ def main():
     print ('Loading monc results:')
     print ('')
     ###1d variables, 2d variables (time,height), 3d variables (time,x,y), 4d variables(time,x,y,z)
-    monc_var_list =[['time_series_2_60','time_series_20_600' ,'z', 'LWP_mean','IWP_mean','SWP_mean','TOT_IWP_mean','GWP_mean'],
-                    ['theta_mean','total_cloud_fraction', 'liquid_cloud_fraction','ice_cloud_fraction',
-                    'vapour_mmr_mean','liquid_mmr_mean','rain_mmr_mean','ice_mmr_mean','snow_mmr_mean',
-                    'graupel_mmr_mean']]
+    #monc_var_list =[['time_series_2_60','time_series_20_600' ,'z', 'LWP_mean','IWP_mean','SWP_mean','TOT_IWP_mean','GWP_mean'],
+    #                ['theta_mean','total_cloud_fraction', 'liquid_cloud_fraction','ice_cloud_fraction',
+    #                'vapour_mmr_mean','liquid_mmr_mean','rain_mmr_mean','ice_mmr_mean','snow_mmr_mean',
+    #                'graupel_mmr_mean']]
     monc_var_list =[['time_series_2_60','time_series_20_600' ,'z','rho', 'LWP_mean','IWP_mean','SWP_mean','TOT_IWP_mean','GWP_mean'],
                     ['theta_mean','total_cloud_fraction', 'liquid_cloud_fraction','ice_cloud_fraction',
                     'vapour_mmr_mean','liquid_mmr_mean','rain_mmr_mean','ice_mmr_mean','snow_mmr_mean',
-                    'graupel_mmr_mean']]#,
+                    'graupel_mmr_mean']]
                 #    ['vwp','lwp','rwp','iwp','swp','gwp','tot_iwp'],
                 #    ['q_vapour','q_cloud_liquid_mass','q_rain_mass','q_ice_mass','q_snow_mass','q_graupel_mass']]
 
@@ -6811,7 +6811,7 @@ def main():
     print ('Loaded!')
 
     ## remove spin up time from monc data
-    monc_data=removeSpinUp(monc_data)
+    monc_data=removeSpinUp(monc_data,monc_spin)
 
     ##################################################################################################################################
     ## -------------------------------------------------------------
