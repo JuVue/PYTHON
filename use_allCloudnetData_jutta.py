b5d752b92b56d99eb726a7e0fd8c72cb6e55bcc5
JuVue <78793837+JuVue@users.noreply.github.com>
2021-06-29 08:13:20 +0100

    removed 3d,4d vfrom monc list

--- use_allCloudnetData_jutta.py
+++ use_allCloudnetData_jutta.py
@@ -6788,9 +6788,9 @@ def main():
     monc_var_list =[['time_series_2_60','time_series_20_600' ,'z','rho', 'LWP_mean','IWP_mean','SWP_mean','TOT_IWP_mean','GWP_mean'],
                     ['theta_mean','total_cloud_fraction', 'liquid_cloud_fraction','ice_cloud_fraction',
                     'vapour_mmr_mean','liquid_mmr_mean','rain_mmr_mean','ice_mmr_mean','snow_mmr_mean',
-                    'graupel_mmr_mean'],
-                    ['vwp','lwp','rwp','iwp','swp','gwp','tot_iwp'],
-                    ['q_vapour','q_cloud_liquid_mass','q_rain_mass','q_ice_mass','q_snow_mass','q_graupel_mass']]
+                    'graupel_mmr_mean']]#,
+                #    ['vwp','lwp','rwp','iwp','swp','gwp','tot_iwp'],
+                #    ['q_vapour','q_cloud_liquid_mass','q_rain_mass','q_ice_mass','q_snow_mass','q_graupel_mass']]
 
 
     print(monc_var_list)
