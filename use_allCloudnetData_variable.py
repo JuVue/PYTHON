94cb29bc62a35e2c243e21e8f225d6e22d34f6c2
JuVue <78793837+JuVue@users.noreply.github.com>
2021-06-30 15:30:23 +0100

    ..

--- use_allCloudnetData_variable.py
+++ use_allCloudnetData_variable.py
@@ -2306,7 +2306,8 @@ def main():
     ### --------------------------------------------------------------------
         print ('Loading multiple diagnostics from cloudnet results:')
         cn_nc0 = {} # observations
-        cn_nc0[c] = Dataset(cn_filename_obs[c],'r')
+        for c in range(0,3):
+            cn_nc0[c] = Dataset(cn_filename_obs[c],'r')
 
         cn_nc1={}
         for m in range(0,len(out_dir)): #UM model data
