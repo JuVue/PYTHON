25dba18d032d9f49ebdccffa6588458178c22378
JuVue <78793837+JuVue@users.noreply.github.com>
2021-07-06 14:05:42 +0100

    ..

--- use_allCloudnetData_variable.py
+++ use_allCloudnetData_variable.py
@@ -1464,8 +1464,8 @@ def main():
     out_dir = ['23_u-cc278_RA1M_CASIM/']
     ### CHOOSE MONC RUNS
     m_out_dir = ['5_control_20180913T0000Z_Wsub-1.5_Fletcher/',
-                '6_control_20180913T0000Z_Wsub-1.5-1km/',
                 '7_20180913T0000Z_Wsub-1.5-1km_solAccum-100_inuc-0_iact-3/',
+                '6_control_20180913T0000Z_Wsub-1.5-1km/',
                 '8_control_20180913T0000Z_Wsub-1.0-1km/',
                 '9_control_20180913T0000Z_Wsub-0.5-1km/']
             #'3_control_20180913T0000Z/',
