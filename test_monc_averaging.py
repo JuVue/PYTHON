a0acfa1462fe8f04ad6b1ed29d088a4547b264bb
JuVue <78793837+JuVue@users.noreply.github.com>
2021-07-09 16:07:20 +0100

    addded 4d averaging

--- test_monc_averaging.py
+++ test_monc_averaging.py
@@ -40,15 +40,16 @@ print ('')
 monc_var_list =[['time_series_2_60','time_series_2_600','time_series_20_600' ,'z','zn','prefn','thref','thinit'],
               ['rho','rhon','theta_mean','w_wind_mean','u_wind_mean','v_wind_mean','total_cloud_fraction', 'liquid_cloud_fraction','ice_cloud_fraction',
               'vapour_mmr_mean','liquid_mmr_mean','rain_mmr_mean','ice_mmr_mean','snow_mmr_mean',
-              'graupel_mmr_mean'],
-              ['q_cloud_liquid_mass']] #,'q_rain_mass','q_ice_mass','q_snow_mass','q_graupel_mass']]
+              'graupel_mmr_mean']]
+
+#separate list for 4 d variables
+monc_var_list_2  = ['q_cloud_liquid_mass','q_rain_mass','q_ice_mass','q_snow_mass','q_graupel_mass']
 
 
 ncm = {}
 monc_data = {}
 print(monc_filename)
 ncm = Dataset(monc_filename,'r')
-embed()
 monc_data={}
 zvar={}
 tvar={}
@@ -82,6 +83,47 @@ monc_data.pop('time_series_2_60')
 monc_data.pop('time_series_2_600')
 monc_data.pop('time_series_20_600')
 
+## averaging 4D variables
+# x = 50m
+# y = 50m
+th = ncm.variables['th'][:]+ ncm.variables['thinit'][0,:]
+p=nncm.variables['prefn'][0,:]
+p = np.array([[[p]*th.shape[2]]*th.shape[1]]*th.shape[0])
+T = calcT(th,p)
+monc_data['p_mean'] = np.nanmean(p,axis=(1,2))
+monc_data['temp_mean'] = np.nanmean(T,axis=(1,2))
+monc_data['th_mean'] = np.nanmean(th,axis=(1,2))
+del(p,T,th)
+
+
+for c in range(0,len(monc_var_list2)):
+    var = monc_var_list2[c]
+    zvar[var]=[]
+    tvar[var]=[]
+    ###getting right z and t dimensions
+    tmp=ncm.variables[var].dimensions
+    if "'z'" in str(tmp):
+        zvar[var]='z'
+    elif "'zn'" in str(tmp):
+        zvar[var]='zn'
+    else:
+        zvar[var]=np.nan
+    if "'time_series_2_60'" in str(tmp):
+        tvar[var]='time1'
+    elif "'time_series_2_600'" in str(tmp):
+        tvar[var]='time2'
+    elif "'time_series_20_600'" in str(tmp):
+        tvar[var]='time3'
+    #calculate mean values
+    tmp = ncm.variables[var][:]
+    tmp[tmp<=0.0]=np.nan
+    monc_data[var +'_mean'] = np.nanmean(tmp,axis=(1,2))
+    monc_data[var +'_mean'][np.isnan(monc_data[var +'_mean'])]=0.0
+    del(tmp)
+
+
+
+
 end = time.time()
 print(end - start)
 print (' Monc data Loaded!')
@@ -110,10 +152,10 @@ embed()
 #     plt.plot(monc_data['time_series_2_600'],dnew[c],'bo')
 #     m+=1
 
-#
-# ### 4D variables
-# vars=['u','v','w','th']
-# embed()
-# for c in vars:
-#     dnew[c.upper() +'_mean'] = np.nanmean(monc_data[c],axis=(1,2))
-#     dnew[c.upper() +'_mean'][np.isnan(dnew[c.upper() +'_mean'])]=0.0
+
+### 4D variables
+vars=['u','v','w','th']
+embed()
+for c in vars:
+    dnew[c.upper() +'_mean'] = np.nanmean(monc_data[c],axis=(1,2))
+    dnew[c.upper() +'_mean'][np.isnan(dnew[c.upper() +'_mean'])]=0.0
