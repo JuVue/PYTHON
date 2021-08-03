6f6d8c59ae815162a09c14b13b27b2d73fc9500c
JuVue <78793837+JuVue@users.noreply.github.com>
2021-08-03 08:49:00 +0100

    added saving of data

--- test_monc_averaging.py
+++ test_monc_averaging.py
@@ -31,7 +31,7 @@ if machine=='LEEDS':
 elif machine=='JASMIN':
     monc_root_dir = '/gws/nopw/j04/ncas_radar_vol1/gillian/MONC/output/'
     m_out_dir = '22_control_20180913T0000Z_qinit2-800m_rand-800m_thForcing-0000-0600_12hTim/'
-
+    monc_exp_dir= '/gws/nopw/j04/ncas_radar_vol1/jutta/MONC/output/'
 monc_filename=glob.glob(monc_root_dir + m_out_dir +'*.nc')
 start = time.time()
 
@@ -39,33 +39,20 @@ start = time.time()
 print ('Loading MONC data:')
 print ('')
 ###1d variables, 2d variables (time,height), 3d variables (time,x,y), 4d variables(time,x,y,z)
-# monc_var_list =[['time_series_2_60','time_series_2_600','time_series_20_600' ,'z','zn', 'LWP_mean','IWP_mean','SWP_mean','TOT_IWP_mean','GWP_mean'],
-#               ['rho','rhon','theta_mean','total_cloud_fraction', 'liquid_cloud_fraction','ice_cloud_fraction',
-#               'vapour_mmr_mean','liquid_mmr_mean','rain_mmr_mean','ice_mmr_mean','snow_mmr_mean',
-#               'graupel_mmr_mean'],
-#               ['vwp','lwp','rwp','iwp','swp','gwp','tot_iwp']]
-#               #['q_vapour','q_cloud_liquid_mass','q_rain_mass','q_ice_mass','q_snow_mass','q_graupel_mass']]
-#separate list for 4 d variables
 
-# monc_var_list =[['time_series_2_60','time_series_2_600','time_series_20_600' ,'z','zn','prefn','thref','thinit'],
-#               ['rho','rhon','theta_mean','w_wind_mean','u_wind_mean','v_wind_mean','total_cloud_fraction', 'liquid_cloud_fraction','ice_cloud_fraction',
-#               'vapour_mmr_mean','liquid_mmr_mean','rain_mmr_mean','ice_mmr_mean','snow_mmr_mean',
-#               'graupel_mmr_mean'],
-#               ['q_cloud_liquid_mass','q_ice_mass','q_snow_mass','q_graupel_mass']]
-
-# monc_var_list =[['time_series_2_60','time_series_2_600','time_series_20_600']
-#                 ['z','zn','prefn','thref','thinit','rho','rhon']
-#                 ['q_cloud_liquid_mass','q_ice_mass','q_snow_mass','q_graupel_mass','u','v','w','th','p']]
-#
-
+# monc_var_list =[['time_series_30_600','time_series_30_60'],
+#                 ['z','zn','prefn','thref','thinit','rho','rhon','theta_mean',
+#                 'liquid_mmr_mean','ice_mmr_mean','snow_mmr_mean','graupel_mmr_mean'],
+#                 ['u','v','w','q','th','p','q_cloud_liquid_mass','q_ice_mass','q_snow_mass','q_graupel_mass']]
 monc_var_list =[['time_series_30_600','time_series_30_60'],
-                ['z','zn','prefn','thref','thinit','rho','rhon','theta_mean',
-                'liquid_mmr_mean','ice_mmr_mean','snow_mmr_mean','graupel_mmr_mean'],
-                ['th','p','q_cloud_liquid_mass','q_ice_mass','q_snow_mass','q_graupel_mass']]
+                ['z','zn','prefn','thref','thinit','rho',
+                'th','p','q_cloud_liquid_mass','q_ice_mass','q_snow_mass','q_graupel_mass'],
+                ['u','v','w','q']]
 
-
-ml2  =        ['liquid_mmr_mean','ice_mmr_mean','snow_mmr_mean','graupel_mmr_mean','model_twc']
 monc_var_avg= ['q_cloud_liquid_mass','q_ice_mass','q_snow_mass','q_graupel_mass','twc_tot']
+
+# ml2  =        ['liquid_mmr_mean','ice_mmr_mean','snow_mmr_mean','graupel_mmr_mean','model_twc']
+
 
 
 ncm = {}
@@ -80,7 +67,10 @@ for c in range(0,len(monc_var_list)):
         var = monc_var_list[c][j]
         zvar[var]=[]
         tvar[var]=[]
-        monc_data[var] = ncm.variables[var][:]
+        if c == len(monc_var_list):
+            monc_data[var+'_mean'] = np.nanmean(ncm.variables[var][:],axis=(1,2))
+        else
+            monc_data[var] = ncm.variables[var][:]
         ###getting right z and t dimensions
         tmp=ncm.variables[var].dimensions
         if "'z'" in str(tmp):
@@ -130,81 +120,20 @@ del(p,T,th)
 
 print('done')
 ###############
-##checking calculations
-viridis = mpl_cm.get_cmap('viridis', 256)
-newcolors = viridis(np.linspace(0, 1, 256))
-greyclr = np.array([0.1, 0.1, 0.1, 0.1])
-newcolors[:10, :] = greyclr
-newcmp = ListedColormap(newcolors)
-
-
-###################
-#### plot air density
-var = 'rho'
-fig=plt.figure(figsize=(6,9))
-plt.subplots_adjust(top = 0.92, bottom = 0.06, right = 0.92, left = 0.08,
-    hspace = 0.4, wspace = 0.2)
-plt.subplot(311)
-ax = plt.gca()
-img=plt.contourf(monc_data[tvar[var]],monc_data[zvar[var]],np.transpose(monc_data[var]),
-    cmap = newcmp)
-cbaxes = fig.add_axes([0.225, 0.96, 0.6, 0.015])
-cb = plt.colorbar(img, cax = cbaxes, orientation = 'horizontal')
-plt.title(var)
-plt.subplot(312)
-plt.contourf(monc_data[tvar[var]],monc_data[zvar[var]],np.transpose(monc_data[var +'_mean']),
-    cmap = newcmp)
-plt.title(var +'_mean')
-plt.subplot(313)
-plt.contourf(monc_data[tvar[var]],monc_data[zvar[var]],np.transpose(monc_data[var ]-monc_data[var +'_mean']),
-    cmap = newcmp)
-plt.title(var +'_diff')
-
-plots_out_dir='./'
-fileout = plots_out_dir + var + '_comparison.png'
-plt.savefig(fileout)
-
-
-clevs=range(265,295,2)
-
-fig=plt.figure(figsize=(6,9))
-plt.subplots_adjust(top = 0.92, bottom = 0.06, right = 0.92, left = 0.08,
-    hspace = 0.4, wspace = 0.2)
-plt.subplot(311)
-ax = plt.gca()
-img=plt.contourf(monc_data[tvar['theta_mean']],monc_data[zvar['theta_mean']],np.transpose(monc_data['th_mean']),
-    levels=clevs,cmap = newcmp)
-cbaxes = fig.add_axes([0.225, 0.96, 0.6, 0.015])
-cb = plt.colorbar(img, cax = cbaxes, orientation = 'horizontal')
-plt.title(' own th_mean calc')
-plt.subplot(312)
-plt.contourf(monc_data[tvar['theta_mean']],monc_data[zvar['theta_mean']],np.transpose(monc_data['theta_mean']),
-    levels=clevs,cmap = newcmp)
-plt.title(var )
-
-clevels=np.arange(-1,1,0.1)
-plt.subplot(313)
-img2=plt.contourf(monc_data[tvar['theta_mean']],monc_data[zvar['theta_mean']],np.transpose(monc_data['th_mean']-monc_data['theta_mean']),
-    levels=clevels,cmap = newcmp)
-cbaxes = fig.add_axes([0.225, 0.35, 0.6, 0.015])
-cb = plt.colorbar(img2, cax = cbaxes, orientation = 'horizontal')
-plt.title(var +'_diff')
-plots_out_dir='./'
-fileout = plots_out_dir +'theta_comparison.png'
-plt.savefig(fileout)
-
-embed()
-#only for checks
-monc_data['model_iwc']= (monc_data['ice_mmr_mean']+monc_data['graupel_mmr_mean']+monc_data['snow_mmr_mean'])*monc_data['rho_mean']
-monc_data['model_lwc']= monc_data['liquid_mmr_mean']*monc_data['rho_mean']
-monc_data['model_twc'] = monc_data['model_lwc'] +monc_data['model_iwc']
-tvar['model_twc']=tvar['ice_mmr_mean']
-zvar['model_twc']= zvar['ice_mmr_mean']
+print('')
+print('calculating twc')
+#monc_data['model_iwc']= (monc_data['ice_mmr_mean']+monc_data['graupel_mmr_mean']+monc_data['snow_mmr_mean'])*monc_data['rho_mean']
+#monc_data['model_lwc']= monc_data['liquid_mmr_mean']*monc_data['rho_mean']
+#monc_data['model_twc'] = monc_data['model_lwc'] +monc_data['model_iwc']
+#tvar['model_twc']=tvar['ice_mmr_mean']
+#zvar['model_twc']= zvar['ice_mmr_mean']
 
 monc_data['twc_tot']=monc_data['q_ice_mass']+monc_data['q_snow_mass']+monc_data['q_graupel_mass']+monc_data['q_cloud_liquid_mass']
 monc_data['twc_tot']=monc_data['twc_tot']*rho
 tvar['twc_tot']=tvar['q_ice_mass']
 zvar['twc_tot']= zvar['q_ice_mass']
+
+print('averaging cloud variables')
 #calculate mean values
 var='q_ice_mass'
 twc_thresh = np.zeros([np.size(monc_data[zvar[var]],0)])
@@ -226,10 +155,6 @@ for c in range(0,len(monc_var_avg)):
     monc_data[var +'_mean']=np.empty((np.size(monc_data[tvar[var]],0),np.size(monc_data[zvar[var]],0)))
     monc_data[var +'_mean'][:]=np.nan
     for t in range(0,np.size(monc_data[tvar[var]],0)):
-        #tt=monc_data[var][t,:]
-        #twc=monc_data['twc_tot'][t,:]
-        #tt[twc<twc_thresh] = np.nan
-        #monc_data[var +'_mean'][t,:] = np.nanmean(tt,axis=(0,1))
         monc_data[var][t,monc_data['twc_tot'][t,:]<twc_thresh] = np.nan
         monc_data[var +'_mean'][t,:] = np.nanmean(monc_data[var][t,:],axis=(0,1))
     monc_data[var +'_mean'][np.isnan(monc_data[var +'_mean'])]=0.0
@@ -238,86 +163,112 @@ for c in range(0,len(monc_var_avg)):
 
 end = time.time()
 print(end - start)
-print (' Monc data Loaded!')
+print (' averaging complete!')
+embed()
+if not os.path.exists(glob.glob(monc_exp_dir + m_out_dir)):
+        os.mkdir(glob.glob(monc_exp_dir + m_out_dir))
+np.save(glob.glob(monc_exp_dir + m_out_dir +'3d_averages.nc'), monc_data)
 
 
-
-viridis = mpl_cm.get_cmap('viridis', 256)
-newcolors = viridis(np.linspace(0, 1, 256))
-greyclr = np.array([0.1, 0.1, 0.1, 0.1])
-newcolors[:10, :] = greyclr
-newcmp = ListedColormap(newcolors)
-
-clevs=[1e-9,1e-8,1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1]
-# clevs=[1e-5,0.25e-4,0.5e-4,0.75e-4,
-#     1e-4,0.25e-3,0.5e-3,0.75e-3,
-#     1e-3,0.25e-2,0.5e-2,0.75e-2,
-#     1e-2,0.25e-1,0.5e-1,0.75e-1,
-#     1e-1,0.25e-0,0.5e-0,0.75e-0,1e-0]
-
-    # fig=plt.figure(figsize=(6,9))
-    # plt.subplots_adjust(top = 0.92, bottom = 0.06, right = 0.92, left = 0.08,
-    #     hspace = 0.4, wspace = 0.2)
-    # plt.subplot(2,1,1)
-    # img=plt.contourf(monc_data[tvar[ml2[c]]],monc_data[zvar[ml2[c]]],np.transpose(monc_data['q_cloud_liquid_mass_mean']),
-    #     levels=clevs, norm = LogNorm(),
-    #     cmap = newcmp)
-    # cbaxes = fig.add_axes([0.225, 0.96, 0.6, 0.015])
-    # cb = plt.colorbar(img, cax = cbaxes, orientation = 'horizontal')
-    # plt.show()
-
-
-for c in range(0,len(monc_var_avg)):
-    var = monc_var_avg[c]
-    fig=plt.figure(figsize=(6,9))
-    plt.subplots_adjust(top = 0.92, bottom = 0.06, right = 0.92, left = 0.08,
-        hspace = 0.4, wspace = 0.2)
-    plt.subplot(2,1,1)
-    ax = plt.gca()
-    img=plt.contourf(monc_data[tvar[ml2[c]]],monc_data[zvar[ml2[c]]],np.transpose(monc_data[ml2[c]]),
-        levels=clevs, norm = LogNorm(),
-        cmap = newcmp)
-    cbaxes = fig.add_axes([0.225, 0.96, 0.6, 0.015])
-    cb = plt.colorbar(img, cax = cbaxes, orientation = 'horizontal')
-    plt.title(ml2[c])
-
-    plt.subplot(2,1,2)
-    plt.contourf(monc_data[tvar[var]],monc_data[zvar[var]],np.transpose(monc_data[var +'_mean']),
-        levels=clevs, norm = LogNorm(),
-        cmap = newcmp)
-    plt.title(var)
-    plots_out_dir='./'
-    fileout = plots_out_dir + var + '_comparison.png'
-    plt.savefig(fileout)
-
+# ######################################################
+# ### plots to check averaging worked
+# ##checking calculations
+# viridis = mpl_cm.get_cmap('viridis', 256)
+# newcolors = viridis(np.linspace(0, 1, 256))
+# greyclr = np.array([0.1, 0.1, 0.1, 0.1])
+# newcolors[:10, :] = greyclr
+# newcmp = ListedColormap(newcolors)
+#
+#
+# ###################
+# #### plot air density
+# var = 'rho'
 # fig=plt.figure(figsize=(6,9))
 # plt.subplots_adjust(top = 0.92, bottom = 0.06, right = 0.92, left = 0.08,
 #     hspace = 0.4, wspace = 0.2)
-# plt.subplot(2,1,1)
+# plt.subplot(311)
 # ax = plt.gca()
-# img=plt.contourf(monc_data[tvar[ml2[c]]],monc_data[zvar[ml2[c]]],np.transpose(monc_data['twc_tot_mean']),
-#     levels=clevs, norm = LogNorm(),
+# img=plt.contourf(monc_data[tvar[var]],monc_data[zvar[var]],np.transpose(monc_data[var]),
 #     cmap = newcmp)
-# plt.show()
-##################################
-#### AVERAGING DATA MANUALLY
-##################################
+# cbaxes = fig.add_axes([0.225, 0.96, 0.6, 0.015])
+# cb = plt.colorbar(img, cax = cbaxes, orientation = 'horizontal')
+# plt.title(var)
+# plt.subplot(312)
+# plt.contourf(monc_data[tvar[var]],monc_data[zvar[var]],np.transpose(monc_data[var +'_mean']),
+#     cmap = newcmp)
+# plt.title(var +'_mean')
+# plt.subplot(313)
+# plt.contourf(monc_data[tvar[var]],monc_data[zvar[var]],np.transpose(monc_data[var ]-monc_data[var +'_mean']),
+#     cmap = newcmp)
+# plt.title(var +'_diff')
 #
-# ### 3D variables
-# dnew={}
-# vars=['lwp','iwp','swp','gwp']
+# plots_out_dir='./'
+# fileout = plots_out_dir + var + '_comparison.png'
+# plt.savefig(fileout)
 #
-# for c in vars:
-#     monc_data[c][monc_data[c]<=0.0]=np.nan
-#     dnew[c.upper() +'_mean'] = np.nanmean(monc_data[c],axis=(1,2))
-#     dnew[c.upper() +'_mean'][np.isnan(dnew[c.upper() +'_mean'])]=0.0
 #
-# vars = list(dnew.keys())
+# clevs=range(265,295,2)
 #
-# tvarid='time_series_2_60'
-# m=1
-# for c in vars:
-#     plt.subplot(len(vars),1,m)
-#     plt.plot(monc_data[tvarid],monc_data[c],'r')
-#     plt.plot(monc_data['time_series_2_600'],dnew[c],'bo')
-#     m+=1
+# fig=plt.figure(figsize=(6,9))
+# plt.subplots_adjust(top = 0.92, bottom = 0.06, right = 0.92, left = 0.08,
+#     hspace = 0.4, wspace = 0.2)
+# plt.subplot(311)
+# ax = plt.gca()
+# img=plt.contourf(monc_data[tvar['theta_mean']],monc_data[zvar['theta_mean']],np.transpose(monc_data['th_mean']),
+#     levels=clevs,cmap = newcmp)
+# cbaxes = fig.add_axes([0.225, 0.96, 0.6, 0.015])
+# cb = plt.colorbar(img, cax = cbaxes, orientation = 'horizontal')
+# plt.title(' own th_mean calc')
+# plt.subplot(312)
+# plt.contourf(monc_data[tvar['theta_mean']],monc_data[zvar['theta_mean']],np.transpose(monc_data['theta_mean']),
+#     levels=clevs,cmap = newcmp)
+# plt.title(var )
+#
+# clevels=np.arange(-1,1,0.1)
+# plt.subplot(313)
+# img2=plt.contourf(monc_data[tvar['theta_mean']],monc_data[zvar['theta_mean']],np.transpose(monc_data['th_mean']-monc_data['theta_mean']),
+#     levels=clevels,cmap = newcmp)
+# cbaxes = fig.add_axes([0.225, 0.35, 0.6, 0.015])
+# cb = plt.colorbar(img2, cax = cbaxes, orientation = 'horizontal')
+# plt.title(var +'_diff')
+# plots_out_dir='./'
+# fileout = plots_out_dir +'theta_comparison.png'
+# plt.savefig(fileout)
+#
+#
+# viridis = mpl_cm.get_cmap('viridis', 256)
+# newcolors = viridis(np.linspace(0, 1, 256))
+# greyclr = np.array([0.1, 0.1, 0.1, 0.1])
+# newcolors[:10, :] = greyclr
+# newcmp = ListedColormap(newcolors)
+#
+# clevs=[1e-9,1e-8,1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1]
+# # clevs=[1e-5,0.25e-4,0.5e-4,0.75e-4,
+# #     1e-4,0.25e-3,0.5e-3,0.75e-3,
+# #     1e-3,0.25e-2,0.5e-2,0.75e-2,
+# #     1e-2,0.25e-1,0.5e-1,0.75e-1,
+# #     1e-1,0.25e-0,0.5e-0,0.75e-0,1e-0]
+#
+#
+# for c in range(0,len(monc_var_avg)):
+#     var = monc_var_avg[c]
+#     fig=plt.figure(figsize=(6,9))
+#     plt.subplots_adjust(top = 0.92, bottom = 0.06, right = 0.92, left = 0.08,
+#         hspace = 0.4, wspace = 0.2)
+#     plt.subplot(2,1,1)
+#     ax = plt.gca()
+#     img=plt.contourf(monc_data[tvar[ml2[c]]],monc_data[zvar[ml2[c]]],np.transpose(monc_data[ml2[c]]),
+#         levels=clevs, norm = LogNorm(),
+#         cmap = newcmp)
+#     cbaxes = fig.add_axes([0.225, 0.96, 0.6, 0.015])
+#     cb = plt.colorbar(img, cax = cbaxes, orientation = 'horizontal')
+#     plt.title(ml2[c])
+#
+#     plt.subplot(2,1,2)
+#     plt.contourf(monc_data[tvar[var]],monc_data[zvar[var]],np.transpose(monc_data[var +'_mean']),
+#         levels=clevs, norm = LogNorm(),
+#         cmap = newcmp)
+#     plt.title(var)
+#     plots_out_dir='./'
+#     fileout = plots_out_dir + var + '_comparison.png'
+#     plt.savefig(fileout)
