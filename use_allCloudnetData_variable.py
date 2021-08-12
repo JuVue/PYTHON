1cc2d803d8655f8655c942f4e32e883dd255b796
JuVue <78793837+JuVue@users.noreply.github.com>
2021-08-12 15:25:14 +0100

    added run 23

--- use_allCloudnetData_variable.py
+++ use_allCloudnetData_variable.py
@@ -2373,8 +2373,8 @@ def main():
     #              '6_control_20180913T0000Z_Wsub-1.5-1km/',
     #              '8_control_20180913T0000Z_Wsub-1.0-1km/',
     #              '9_control_20180913T0000Z_Wsub-0.5-1km/']
-    m_out_dir =['22_control_20180913T0000Z_qinit2-800m_rand-800m_thForcing-0000-0600_12hTim/']
-    m_out_dir =['23_20180913T0000Z_6hSpin-up_12h0600-0000thTend/']
+    m_out_dir =['22_control_20180913T0000Z_qinit2-800m_rand-800m_thForcing-0000-0600_12hTim/',
+                '23_20180913T0000Z_6hSpin-up_12h0600-0000thTend/']
             #'4_control_20180913T0000Z_Wsub-1.5/',
     #################################################################
     ## create labels for figure legends - done here so only needs to be done once!
