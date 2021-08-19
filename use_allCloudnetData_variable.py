7caa953a36dec0e2752b5a4d1feadf3334dc9709
gyoung410 <G.Young1@leeds.ac.uk>
2021-08-19 11:57:41 +0100

    forgot ,

--- use_allCloudnetData_variable.py
+++ use_allCloudnetData_variable.py
@@ -2389,8 +2389,8 @@ def main():
     #              '8_control_20180913T0000Z_Wsub-1.0-1km/',
     #              '9_control_20180913T0000Z_Wsub-0.5-1km/']
     m_out_dir =[#'22_control_20180913T0000Z_qinit2-800m_rand-800m_thForcing-0000-0600_12hTim/']
-               '23_20180913T0000Z_6hSpin-up_12h0600-0000thTend_20h1200-0600thTend/'
-               # '24_20180913T0000Z_12h0600-0000_20h1200-0600thTend_0.1Cooper/'
+               '23_20180913T0000Z_6hSpin-up_12h0600-0000thTend_20h1200-0600thTend/',
+               # '24_20180913T0000Z_12h0600-0000_20h1200-0600thTend_0.1Cooper/',
                '25_20180913T0000Z_20h0600-0000thTend/'
             #'4_control_20180913T0000Z_Wsub-1.5/',
             ]
@@ -2471,6 +2471,7 @@ def main():
     for m in range(0, len(m_out_dir)):
         fname=glob.glob(monc_root_dir + m_out_dir[m] +'*.nc')
         monc_filename.append(fname)
+
     monc_3d_filename=[]
     for m in range(0, len(m_out_dir)):
         fname=glob.glob(monc_avg_dir + m_out_dir[m] +'3d*npy')
