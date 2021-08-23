2a05c998fea8a42ea9b51d3b00124b4a7723fb6c
gyoung410 <G.Young1@leeds.ac.uk>
2021-08-23 12:12:30 +0100

    height

--- use_allCloudnetData_variable.py
+++ use_allCloudnetData_variable.py
@@ -1498,7 +1498,7 @@ def plot_lwcProfiles_split(obs_data,lwcvar,lwcstr, thresholding, plots_out_dir,d
     fcolsmonc=['navajowhite','goldenrod','moccasin','lightsalmon','lightcoral']
     ### define axis instance
     ####LWC
-    plt.figure(figsize=(14,8))
+    plt.figure(figsize=(14,7))
     plt.subplots_adjust(top = 0.9, bottom = 0.1, right = 0.98, left = 0.08)
 
     for pt in range(0,len(prof_time)):
